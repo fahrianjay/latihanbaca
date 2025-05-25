daae9ba977b2c70057ed52ea61c13315cbfd2560
fahrianjay <fahriansyahfahriansyah196@gmail.com>
2025-05-25 13:11:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("selamat datang di web belajar membaca")
 st.write(
     "belajar membaca seru bersama pahri si ganteng"
 )
-st.image("
+st.image("Screenshot_20250525-130209_Samsung Notes.jpg", width=200)
